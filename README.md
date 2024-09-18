784b2c2e4f0cb0d2c8d2a79972a3ef26fe02f047
Max Weidauer <max+git@weidauer.art>
2024-09-18 17:30:12 +0200

    implemented scaling by actual monitor size

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Wallcrop is a simple tool for cropping an image into smaller images for use as individual desktop wallpapers in a multi-monitor setup.
 
+It can either arrange by size in pixels, or by the actual size of the monitors in cm.
+
 ## Installation:
 
 The easiest way to install would be using `pip` in a virtual environment:
@@ -16,7 +18,8 @@ pip install wallcrop
 
 In order to crop up some images you need to let wallcrop know about your monitor setup. This is done in a yaml file, here called `monitors.yml`, an example for this file can be found in `examples/monitors.yml`.
 
-The `monitors.yml` contains a list of monitors, each with a `name`, `height` and `width` of the monitor (if your monitor is vertical just swap these) and `x` and `y` position of the top left corner of the monitor in pixels
+The `monitors.yml` contains a list of monitors, each with a `name`, `height` and `width` of the monitor (if your monitor is vertical just swap these) and `x` and `y` position of the top left corner of the monitor in pixels.
+If you want to use the `actual-monitor-size` option (arranging not by the size in pixels, but the actual size of the screens in cm) you need to additionally add values for `x_cm`, `y_cm`, `width_cm` and `height_cm` for each monitor. Gaps between monitors should be accounted for in `x_cm` and `y_cm`.
 
 To crop all images in a folder use
 
--- src/wallcrop/wallcrop.py
+++ src/wallcrop/wallcrop.py
@@ -5,7 +5,7 @@ from PIL import Image, UnidentifiedImageError
 from math import ceil
 import click
 from tqdm import tqdm
-
+import sys
 
 
 @click.command("wallcrop",
@@ -25,6 +25,7 @@ context_settings=dict(help_option_names=["-h", "--help"]),)
     is_flag=True,
     help="Disables Scaling of Input Image. If Image is too small, it will be skipped",
 )
+@click.option("-a", "--actual-monitor-sizes", is_flag=True, help="crop using actual monitor sizes, not just to make pixels fit")
 @click.option(
     "-o",
     "--output",
@@ -37,22 +38,46 @@ context_settings=dict(help_option_names=["-h", "--help"]),)
     "-f", "--format", type=click.STRING, default="png", help="file format of the cropped images",show_default=True
 )
 @click.argument("input_file", nargs=-1, type=click.Path(exists=True, path_type=Path))
-def main(monitor_file: Path, output_path: Path, no_scale: bool, input_file: tuple[Path], format: str):
+def main(monitor_file: Path, output_path: Path, no_scale: bool, input_file: tuple[Path], format: str, actual_monitor_sizes: bool):
     with open(monitor_file) as f:
         monitors = yaml.safe_load(f)
 
-    min_x = min([m["x"] for m in monitors])
-    min_y = min([m["y"] for m in monitors])
-    max_x = max([m["x"] + m["width"] for m in monitors])
-    max_y = max([m["y"] + m["height"] for m in monitors])
+    # calculate necessary source image size
+    if actual_monitor_sizes:
+        try:
+            max_pixels_per_cm_x = max([m["width"]/m["width_cm"] for m in monitors])
+            max_pixels_per_cm_y = max([m["height"]/m["height_cm"] for m in monitors])
+
+            min_x = int(min([m["x_cm"] * max_pixels_per_cm_x for m in monitors]))
+            min_y = int(min([m["y_cm"]  * max_pixels_per_cm_y for m in monitors]))
+            max_x = int(max([(m["x_cm"] + m["width_cm"]) * max_pixels_per_cm_x for m in monitors]))
+            max_y = int(max([(m["y_cm"] + m["height_cm"]) * max_pixels_per_cm_y for m in monitors]))
+        except KeyError:
+            tqdm.write(f"ERROR: in order to use the actual monitor size option, each monitor needs the keys width (in px), height (in px), width_cm, height_cm, x_cm, y_cm")
+            sys.exit(-1)
+            
+        tqdm.write(f"pixels per cm: ({max_pixels_per_cm_x}x{max_pixels_per_cm_y})")
+
+        for m in monitors:
+            tqdm.write(f"{m["name"]}")
+            tqdm.write(f"scaled size {m['width_cm']*max_pixels_per_cm_x}x{m['height_cm']*max_pixels_per_cm_y}")
+    else:
+        min_x = min([m["x"] for m in monitors])
+        min_y = min([m["y"] for m in monitors])
+        max_x = max([m["x"] + m["width"] for m in monitors])
+        max_y = max([m["y"] + m["height"] for m in monitors])
 
     total_width, total_height = max_x - min_x, max_y - min_y
-    tqdm.write(f"total screen area: {total_width}x{total_height}")
+    tqdm.write(f"total screen area: {total_width}x{total_height} px")
 
+    # iterate over all images
     for image_path in tqdm(input_file):
+
+        # ignore directories
         if not image_path.is_file():
             continue
 
+        # ignore invalid image files
         try:
             im = Image.open(image_path)
         except UnidentifiedImageError:
@@ -63,7 +88,9 @@ def main(monitor_file: Path, output_path: Path, no_scale: bool, input_file: tupl
         if no_scale and not (width >= total_width and height >= total_height):
             tqdm.write(f"image {image_path} is to small, skipping...")
             continue
+
         elif not no_scale:
+            # scale image if allowed
             scale_x = total_width / width
             scale_y = total_height / height
             scale = max(scale_x, scale_y)
@@ -73,18 +100,30 @@ def main(monitor_file: Path, output_path: Path, no_scale: bool, input_file: tupl
                 tqdm.write(f"scaled {image_path.stem} {scaling_direcion} to {im.size}, scaling factor = {scale:.02}")
 
         width, height = im.size
+
+        # calculate offsets so image is nicely centered
         offset_x = int(width / 2 - total_width / 2) - min_x
         offset_y = int(height / 2 - total_height / 2) - min_y
 
         os.makedirs(output_path / image_path.stem, exist_ok=True)
 
+        # iterate over the monitors
         for m in monitors:
-            x_l = offset_x + m["x"]
-            x_r = x_l + m["width"]
+            if actual_monitor_sizes:
+                x_l = offset_x + int(m["x_cm"] * max_pixels_per_cm_x)
+                x_r = x_l + int(m["width_cm"] * max_pixels_per_cm_x)
 
-            y_u = offset_y + m["y"]
-            y_l = y_u + m["height"]
-            cropped = im.crop((x_l, y_u, x_r, y_l))
+                y_u = offset_y + int(m["y_cm"] * max_pixels_per_cm_y)
+                y_l = y_u + int(m["height_cm"] * max_pixels_per_cm_y)
+                cropped = im.crop((x_l, y_u, x_r, y_l))
+                cropped = cropped.resize((m["width"], m['height']))
+            else:
+                x_l = offset_x + m["x"]
+                x_r = x_l + m["width"]
+
+                y_u = offset_y + m["y"]
+                y_l = y_u + m["height"]
+                cropped = im.crop((x_l, y_u, x_r, y_l))
 
             filename = f"{image_path.stem}_{m["name"]}.{format}"
             cropped.save(output_path / image_path.stem / filename)
